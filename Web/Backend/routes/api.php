f68bbd0f4ecd99a93bc13512dbe49573ed2b9f38
juan-e-caro <juaneduardocarosochimilca.jecs@gmail.com>
2025-11-24 19:01:02 -0500

    Add files via upload

--- Web/Backend/routes/api.php
+++ Web/Backend/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
